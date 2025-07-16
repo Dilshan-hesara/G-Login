23a22c478d60306ae0c98a1370f257156312a0d6
Dilshan-hesara <hesarsdilshan@gmail.com>
2025-07-16 20:16:40 +0530

    auth contrlloer

--- src/main/java/com/dilshan/login/service/CustomUserDetailsService.java
+++ src/main/java/com/dilshan/login/service/CustomUserDetailsService.java
@@ -16,7 +16,6 @@ public class CustomUserDetailsService implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-        return userRepository.findByEmail(email)
-                .orElseThrow(() -> new UsernameNotFoundException("User not found with email: " + email));
+        return userRepository.findByEmail(email).orElseThrow(() -> new UsernameNotFoundException("User not found with email: " + email));
     }
 }
